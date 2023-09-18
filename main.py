27229befc6c8e136532156b72b65c71043230127
Jamoladdin <djamazar23@gmail.com>
2023-09-18 17:30:47 +0200

    popitka1 game

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-a = 44
-b = 23
-print(a + b)
-print('hello')
-print('bye')
+import random
+
+guess_word = ["adidas", "nike", "puma"]
+rand_words = random.choice(guess_word)
+kolicestvo_ugad = int(input("kolicestvo popitok?: "))
+print(kolicestvo_ugad,r" Popitok ugadat slovo")
+print("vopros: samie izvestnie mirovie brands")
+
+
+while guess_word:
+    ugaday_slovo = input("ugadayte bukvu ili slovo!: ")
+
+    if ugaday_slovo in rand_words:
+        print("vy ugadali")
+    else:
+        print("ne ugadali, sprobuyte we")
+        if ugaday_slovo.find(rand_words):
+            print(rand_words, r"ss")
